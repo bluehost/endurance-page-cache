ff0e56538950c47cc03d307e1728f1f80d17f69a
Mike <web.mhansen@gmail.com>
2017-03-22 16:54:17 -0600

    schedule specific request not a purge_all.

--- endurance-page-cache.php
+++ endurance-page-cache.php
@@ -170,9 +170,9 @@ if ( ! class_exists( 'Endurance_Page_Cache' ) ) {
 		function purge_request( $uri ) {
 			$purged = get_transient( 'epc_purged_' . md5( $uri ) );
 			if ( true === $purge_request && true == $purged ) {
-				$next = wp_next_scheduled( array( $this, 'purge_all' ) );
+				$next = wp_next_scheduled( array( $this, 'purge_request' ), $uri );
 				if ( false == $next ) {
-					wp_schedule_single_event( time() + 180, array( $this, 'purge_all' ), $uri );
+					wp_schedule_single_event( time() + 180, array( $this, 'purge_request' ), $uri );
 				}
 				return;
 			}
