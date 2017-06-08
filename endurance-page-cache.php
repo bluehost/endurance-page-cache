6eba88bb53c2a6aa063ee37350d2a82340cd3aaf
Mike <web.mhansen@gmail.com>
2017-06-08 10:36:51 -0600

    No longer schedule a cron to clean up files.

--- endurance-page-cache.php
+++ endurance-page-cache.php
@@ -24,9 +24,6 @@ if ( ! class_exists( 'Endurance_Page_Cache' ) ) {
 			$this->cache_level = get_option( 'endurance_cache_level', 2 );
 			$this->cache_dir = WP_CONTENT_DIR . '/endurance-page-cache';
 			$this->cache_exempt = array( 'wp-admin', '.', 'checkout', 'cart', 'wp-json', '%', '=', '@', '&', ':', ';' );
-			if ( ! wp_next_scheduled( 'epc_purge' ) ) {
-				wp_schedule_event( time() + ( HOUR_IN_SECONDS * 2 ), 'epc_weekly', 'epc_purge' );
-			}
 		}
 
 		function hooks() {
