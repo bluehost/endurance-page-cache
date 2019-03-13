3198fa32325d8f5c958fe7ba03f953a6c0513c6c
Micah Wood <micah@wpscholar.com>
2019-03-13 16:21:53 -0400

    Bump to version 1.5

--- endurance-page-cache.php
+++ endurance-page-cache.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Endurance Page Cache
  * Description: This cache plugin is primarily for cache purging of the additional layers of cache that may be available on your hosting account.
- * Version: 1.4
+ * Version: 1.5
  * Author: Mike Hansen
  * Author URI: https://www.mikehansen.me/
  * License: GPLv2 or later
@@ -16,7 +16,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'EPC_VERSION', 1.4 );
+define( 'EPC_VERSION', 1.5 );
 
 if ( ! class_exists( 'Endurance_Page_Cache' ) ) {
 
