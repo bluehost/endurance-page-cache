77cad7b84596ce6c2d73440b0812a3a61bcbc50d
William Earnhardt <wearnhardt@gmail.com>
2019-05-23 12:35:45 -0400

    Bump version to 1.6

--- endurance-page-cache.php
+++ endurance-page-cache.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Endurance Page Cache
  * Description: This cache plugin is primarily for cache purging of the additional layers of cache that may be available on your hosting account.
- * Version: 1.5
+ * Version: 1.6
  * Author: Mike Hansen
  * Author URI: https://www.mikehansen.me/
  * License: GPLv2 or later
