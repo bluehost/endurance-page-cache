d180aec4ce128915047a475c3eb2dc757033ef68
David Ryan <0aveRyan@users.noreply.github.com>
2019-12-04 12:55:02 -0700

    Version 1.7 (#77)
    * Updated coding standards
    * Updated how throttling works and where it is triggered.
    * Added unit test for the should_throttle() method.
    * Ensure that throttling is specific to the method doing the throttling.
    * cache purge with buffer
    * add gpl license
    * license and platform restriction check

---
+++ tests/ShouldThrottleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class ShouldThrottleTest
+ */
+class ShouldThrottleTest extends WP_UnitTestCase {
+
+	/**
+	 * @var Endurance_Page_Cache
+	 */
+	protected $instance;
+
+	/**
+	 * Setup class instance
+	 */
+	public function setUp() {
+		$this->instance = new Endurance_Page_Cache();
+	}
+
+	/**
+	 * Tests the should_throttle() method.
+	 */
+	public function test_should_throttle() {
+
+		$uri = 'https://www.google.com';
+
+		$this->assertEquals( false, $this->instance->should_throttle( $uri, 'page' ), 'Failed asserting that should_throttle() returned false.' );
+		$this->assertEquals( true, $this->instance->should_throttle( $uri, 'page' ), 'Failed asserting that should_throttle() returned true.' );
+
+		$this->assertEquals( false, $this->instance->should_throttle( $uri . '/', 'page' ), 'Failed asserting that should_throttle() returned false.' );
+		$this->assertEquals( false, $this->instance->should_throttle( $uri . '/', 'other' ), 'Failed asserting that should_throttle() returned false.' );
+
+	}
+
+}
