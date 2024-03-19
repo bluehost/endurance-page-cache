b1e7bdbb10dcf821f49fb8c679344530c7393055
arunshenoy99 <devarunshenoy99@gmail.com>
2024-03-19 14:11:17 +0530

    Move the null check before hashing.

--- endurance-page-cache.php
+++ endurance-page-cache.php
@@ -756,13 +756,13 @@ if ( ! class_exists( 'Endurance_Page_Cache' ) ) {
 		 */
 		public function should_throttle( $uri, $type ) {
 
-			if ( is_null( $uri ) ) {
-				return true;
-			}
-
 			$should_throttle = false;
 
 			$this->should_update_throttled_items = true;
+
+			if ( is_null( $uri ) ) {
+				return true;
+			}
 
 			$hash = md5( $uri );
 
